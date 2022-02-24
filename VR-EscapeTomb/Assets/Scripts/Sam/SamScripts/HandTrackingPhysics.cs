dc3b0d00c9918b192d1e7cf6b33d6ef02580f6c3
Samuel <sk4287n@gre.ac.uk>
2022-02-24 12:51:35 +0000

    Sam Stuff

---
+++ VR-EscapeTomb/Assets/Scripts/Sam/SamScripts/HandTrackingPhysics.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HandTrackingPhysics : MonoBehaviour
+{
+    public Transform target;
+    private Rigidbody rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // position
+        rb.velocity = (target.position - transform.position) / Time.fixedDeltaTime;
+        //rotation
+        Quaternion rotationDifference = target.rotation * Quaternion.Inverse(transform.rotation);
+        rotationDifference.ToAngleAxis(out float angleInDegree, out Vector3 roatationAxis);
+
+        Vector3 rotationDifferenceInDegree = angleInDegree * roatationAxis;
+        rb.angularVelocity = (rotationDifferenceInDegree * Mathf.Deg2Rad / Time.fixedDeltaTime);
+    }
+}
---
+++ VR-EscapeTomb/Assets/Scripts/Sam/SamScripts/RightController.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+
+namespace Controllers.Right
+{
+    public class RightController : MonoBehaviour
+    {
+
+
+        private GameObject touchingObj;
+        public bool isTouching;
+        public bool trigerValue;
+        private bool Value;
+        [SerializeField]
+        private Animator h_Animator;
+        private const string ANIMATOR_GRIP = "Grip";
+        private float h_GripTarget = 1.0f;
+        private float h_CurGrip = 0.0f;
+        public float h_speed = 8.0f;
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            var rightHandController = new List<InputDevice>();
+            var disiredCharacteristicsR = InputDeviceCharacteristics.HeldInHand | InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller;
+            InputDevices.GetDevicesWithCharacteristics(disiredCharacteristicsR, rightHandController);
+
+            foreach (var device in rightHandController)
+            {
+
+                Vector3 position;
+                if (device.TryGetFeatureValue(CommonUsages.devicePosition, out position))
+                {
+                    transform.localPosition = position;
+                }
+                Quaternion orientation;
+                if (device.TryGetFeatureValue(CommonUsages.deviceRotation, out orientation))
+                {
+                    transform.localRotation = orientation;
+                }
+
+
+                if (device.TryGetFeatureValue(CommonUsages.gripButton, out trigerValue) && trigerValue)
+                {
+                    Debug.Log("Trigger button was pressed");
+                    if (touchingObj != null)
+                    {
+                        touchingObj.transform.position = transform.position;
+                        touchingObj.transform.rotation = transform.rotation;
+                    }
+                }
+                // checking if grip button is used.
+                if (device.TryGetFeatureValue(CommonUsages.gripButton, out Value))
+                {
+                    Debug.Log("print" + Value);
+                    // if the button is used then the animation will start playing 
+                    if (Value != false)
+                    {
+                        h_CurGrip = Mathf.MoveTowards(h_CurGrip, 1f, Time.deltaTime * h_speed);
+                        h_Animator.SetFloat(ANIMATOR_GRIP, h_CurGrip);
+
+                    }
+                    else
+                    {
+                        h_CurGrip = Mathf.MoveTowards(h_CurGrip, 0f, Time.deltaTime * h_speed);
+                        h_Animator.SetFloat(ANIMATOR_GRIP, h_CurGrip);
+                    }
+                }
+            }
+
+
+        }
+        public void OnTriggerEnter(Collider other)
+        {
+            touchingObj = other.gameObject;
+            isTouching = true;
+
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            touchingObj = null;
+            isTouching = false;
+
+        }
+
+    }
+
+
+}
+
+
+
