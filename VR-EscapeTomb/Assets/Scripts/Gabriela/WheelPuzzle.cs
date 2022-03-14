003403f2e05c46d6e2abec822cdcb2e43e6cb9ef
gabpyj <72604501+gabpyj@users.noreply.github.com>
2022-03-14 12:58:20 +0000

    teleport + grabbing test scene

---
+++ VR-EscapeTomb/Assets/Scripts/Gabriela/WheelPuzzle.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class WheelPuzzle : MonoBehaviour
+{
+    [SerializeField] private GameObject wheel;
+    [SerializeField] private float targetRot_z = 60;
+    [SerializeField] private float turnSpeed = 1f;
+
+    private bool canRotate;
+    private float t;
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Hands")
+        {
+            canRotate = true;
+            {
+                if (canRotate == true)
+                {
+                    StartCoroutine(Turn());
+                }
+            }
+        }
+    }
+
+    private IEnumerator Turn()
+    {
+        Quaternion rot_old = wheel.transform.rotation;
+        wheel.transform.Rotate(0, 0, targetRot_z);
+        Quaternion rot_new = wheel.transform.rotation;
+
+        for (t = 0.0f; t <= 1.0f; t += (turnSpeed * Time.deltaTime))
+        {
+            wheel.transform.rotation = Quaternion.Slerp(rot_old, rot_new, t);
+            yield return null;
+        }
+
+        wheel.transform.rotation = rot_new;
+        canRotate = false;
+    }
+
+}
