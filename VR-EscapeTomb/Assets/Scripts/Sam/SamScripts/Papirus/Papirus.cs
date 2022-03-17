071c18e1368610c61236a78da4a8bb61c0beb48b
Samuel <sk4287n@gre.ac.uk>
2022-03-17 12:01:19 +0000

    Papirus and Quest 4

---
+++ VR-EscapeTomb/Assets/Scripts/Sam/SamScripts/Papirus/Papirus.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName ="Item/Create New Item")]
+public class Papirus : ScriptableObject
+{
+    public int id;
+    public string itemName;
+}
