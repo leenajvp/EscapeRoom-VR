ada1f696641b7084fbfe83c0ba93e82014d79e6d
Leena Jarvenpaa <leena.jarvenpaa@windowslive.com>
2022-03-28 17:08:15 +0100

    throw debug

--- VR-EscapeTomb/Assets/Scripts/Leena/ThrowingPuzzle/ThrowBalls.cs
+++ VR-EscapeTomb/Assets/Scripts/Leena/ThrowingPuzzle/ThrowBalls.cs
@@ -25,12 +25,10 @@ public class ThrowBalls : MonoBehaviour
         {
             if (velocity.z > -0.5f || velocity.z < -0.5f || velocity.x > -0.5f || velocity.x < -0.5f || velocity.y > 0.5f || velocity.y < -0.5f)
             {
-                rb.mass += 5f;
+                rb.mass += 100f;
 
                 if (rb.drag <= maxDarg)
-                    rb.drag += 0.1f;
-
-                Debug.Log("speed");
+                    rb.drag += 0.5f;
             }
 
             if (velocity.z < -4f || velocity.x > -4f)
